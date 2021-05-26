cdb624e5f008d1538e4021ebe16535c320afe83c
Haemish Graham <hagraham@cisco.com>
2021-05-26 17:11:19 -0400

    Update application to use new msxswagger.

--- python-hello-world-service-6/app.py
+++ python-hello-world-service-6/app.py
@@ -2,47 +2,31 @@
 # Copyright (c) 2021 Cisco Systems, Inc and its affiliates
 # All Rights reserved
 #
-from flask import Flask, Response
-from flask_restplus import Api
-from msxswagger import MSXSwaggerConfig, Security, DocumentationConfig
+from flask import Flask
+from msxswagger import MSXSwaggerConfig, Security, DocumentationConfig, Sso
 
 from controllers.items_controller import ItemsApi, ItemApi
 from controllers.languages_controller import LanguageApi, LanguagesApi
 
+
 app = Flask(__name__)
-api = Api(app)
-api.add_resource(ItemsApi, "/helloworld/api/v1/items")
-api.add_resource(ItemApi, "/helloworld/api/v1/items/<id>")
-api.add_resource(LanguagesApi, "/helloworld/api/v1/languages")
-api.add_resource(LanguageApi, "/helloworld/api/v1/languages/<id>")
-
-
-# Configure Swagger Documentation
-@app.route('/helloworld/swagger.json')
-def swagger_json():
-	with app.open_resource("swagger.json", "r") as file:
-		content = file.read()
-	return Response(
-		content,
-		mimetype='application/json',
-		headers={'Content-Disposition': 'attachment;filename=swagger.json'})
-
+sso = Sso(base_url='https://dev-plt-aio1.lab.ciscomsx.com/idm',
+          client_id='local-public-client2')
 
 documentation_config = DocumentationConfig(
-	'/helloworld',
-	'/helloworld/swagger.json',
-	'/helloworld/swagger',
-	'3.0.0',
-	Security(False))
+	root_path='/helloworld',
+	security=Security(False, sso))
 
 swagger = MSXSwaggerConfig(
 	app,
-	None,
 	documentation_config,
-	disable_swagger_json_generation=True)
+	swagger_resource="swagger.json")
 
+swagger.api.add_resource(ItemsApi, "/api/v1/items")
+swagger.api.add_resource(ItemApi, "/api/v1/items/<id>")
+swagger.api.add_resource(LanguagesApi, "/api/v1/languages")
+swagger.api.add_resource(LanguageApi, "/api/v1/languages/<id>")
 app.register_blueprint(swagger.api.blueprint)
-
 
 if __name__ == '__main__':
 	app.run()
