2de7dac42378e0bf123f7d35c7c22a45e3c85853
Haemish Graham <hagraham@cisco.com>
2021-06-14 11:58:44 -0400

    Added Python Vault example.

--- python-hello-world-service-4/config.py
+++ python-hello-world-service-4/config.py
@@ -2,12 +2,14 @@
 # Copyright (c) 2021 Cisco Systems, Inc and its affiliates
 # All Rights reserved
 #
+import logging
 import pkgutil
 from os import environ
 from collections import namedtuple
 import yaml
 
 ConsulConfig = namedtuple("ConsulConfig", ["host", "port", "cacert"])
+VaultConfig = namedtuple("VaultConfig", ["scheme", "host", "port", "token", "cacert"])
 
 
 class Config(object):
@@ -21,4 +23,9 @@ class Config(object):
         config["consul"]["port"] = environ.get("SPRING_CLOUD_CONSUL_PORT", config["consul"]["port"])
         self.consul = ConsulConfig(**config["consul"])
 
-
+        # Apply environment variables and create Vault config object.
+        config["vault"]["scheme"] = environ.get("SPRING_CLOUD_VAULT_SCHEME", config["vault"]["scheme"])
+        config["vault"]["host"] = environ.get("SPRING_CLOUD_VAULT_HOST", config["vault"]["host"])
+        config["vault"]["port"] = environ.get("SPRING_CLOUD_VAULT_PORT", config["vault"]["port"])
+        config["vault"]["token"] = environ.get("SPRING_CLOUD_VAULT_TOKEN", config["vault"]["token"])
+        self.vault = VaultConfig(**config["vault"])
